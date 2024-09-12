18bdcbb7af4922bf86c31bab2f75370943ef874f
GalipYanar <qalipynrr@gmail.com>
2024-09-12 21:29:02 +0300

    added brad/cars controller and create brands

---
+++ src/main/java/com/example/RentACar/controller/BrandController.java
@@ -0,0 +1,27 @@
+package com.example.RentACar.controller;
+
+import com.example.RentACar.model.Brand;
+import com.example.RentACar.service.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/brand")
+@RestController
+
+public class BrandController {
+
+    @Autowired
+    private BrandService brandService;
+
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @PostMapping("/create")
+    public ResponseEntity<Brand> createBrand(@RequestBody Brand brand){
+        return new ResponseEntity<>(brandService.createBrand(brand), HttpStatus.CREATED);
+    }
+}
